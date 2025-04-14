0c02bffbb7ada1f1bef8241a282ce75909010569
valarious890 <vatsananan2546@gmail.com>
2025-04-14 12:17:35 +0000

    update

--- pages/Music_Only.py
+++ pages/Music_Only.py
@@ -1,2 +1,4 @@
+import streamlit as st
+
 st.set_page_config(page_title="Music", layout="wide")
 st.title("🎵 Music Only Page")
--- pages/Thriller_Only.py
+++ pages/Thriller_Only.py
@@ -1,2 +1,4 @@
+import streamlit as st
+
 st.set_page_config(page_title="Thriller", layout="wide")
 st.title("🎵 Thriller Only Page")
